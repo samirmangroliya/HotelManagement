ffc02ba4ef75487aa2ec927c9b067349812480d2
samir <sbmangroliya2012@gmail.com>
2026-03-07 22:46:16 +0530

    Server Host Changes...

--- server/src/main/kotlin/com/samir/hotelmanagement/Application.kt
+++ server/src/main/kotlin/com/samir/hotelmanagement/Application.kt
@@ -17,7 +17,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = SERVER_PORT, host = SERVER_HOST, module = Application::module)
         .start(wait = true)
 }
 
--- shared/src/commonMain/kotlin/com/samir/hotelmanagement/Constants.kt
+++ shared/src/commonMain/kotlin/com/samir/hotelmanagement/Constants.kt
@@ -1,3 +1,4 @@
 package com.samir.hotelmanagement
 
 const val SERVER_PORT = 8081
+const val SERVER_HOST = "0.0.0.0"
